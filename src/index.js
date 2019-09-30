5234abc57a9eb3dfbc62a49d8ec7f795c64725da
VukZh <vukfx@inbox.ru>
2019-09-30 15:46:56 +0600

    change index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus = "family",
+  knowsProgramming = true,
+  config = { family: 4 }
+) {
+  const findFactor = config[focus];
+  // console.log(" -- " + findFactor);
+  return knowsProgramming
+    ? Math.ceil(800 / findFactor)
+    : Math.ceil(1300 / findFactor);
+};
